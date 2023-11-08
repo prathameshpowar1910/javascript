026bd51b453be2418b0b3c50083411d042ee9f47
Prathamesh Powar <ppowar1910@gmail.com>
2023-11-08 19:30:15 +0530

    Refactor code to use reduce method for shopping cart total calculation

--- mfr.js
+++ mfr.js
@@ -10,7 +10,7 @@ const newMarks = marks
   .map((mark) => mark - 10) // [10, 30, 50, 70, 90]
   .filter((mark) => mark > 30); // [50, 70, 90]
 
-console.log(newMarks)
+//console.log(newMarks);
 
 //* filter method
 
@@ -21,3 +21,47 @@ const seniorCitizensUnder70 = ages.filter((age) => {
   return age >= 60 && age <= 70;
 });
 // console.log(seniorCitizensUnder70)
+
+//* reduce method
+
+const numbers = [10, 20, 30, 40, 50];
+
+const initialValue = 0;
+
+// const sum = numbers.reduce(
+//   (accumulator, currentValue) => accumulator + currentValue,
+//   initialValue
+// );
+
+const sum = numbers.reduce((acc, curr) => {
+  // console.log(`acc: ${acc}, curr: ${curr}`);
+  return acc + curr;
+}, initialValue);
+
+// console.log(sum);
+
+const shoppingCart = [
+  {
+    product: "phone",
+    qty: 1,
+    price: 10000,
+  },
+  {
+    product: "screen guard",
+    qty: 3,
+    price: 100,
+  },
+  {
+    product: "case",
+    qty: 2,
+    price: 500,
+  },
+];
+
+const cartTotal = shoppingCart.reduce((acc,item)=> acc + item.price*item.qty, 0)
+
+// const cartTotal = shoppingCart
+//   .map((prod) => prod.qty * prod.price)
+//   .reduce((acc, curr) => acc + curr, 0);
+
+// console.log(cartTotal);
